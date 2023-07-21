1a342a284f7d1e5dd018b42189aa06df74aa2e4b
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-07-21 15:42:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function new(){
+	return new Promise(function (resolve, reject) {
+          
+                // Setting 2000 ms time
+                setTimeout(resolve, 1000);
+            }).then(function () {
+                document.getElementById("output").innerHTML = 
+                "Wrapped setTimeout after 1000ms";
+            });
+        }
+}
